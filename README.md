4e18337a7f658e36cad8f0d5cc1d032c3f874b25
philip koivunen <philip.koivunen@outlook.com>
2022-06-10 15:09:00 +0200

    implemented some rules for eslint

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # eslint-config-nfler
 Eslint config for nfler js
+
+
+## usage local
+```
+  npm link ../eslint-config-nfler
+  and
+  npm unlink ../eslint-config-nfler
+```
+
+```
+  npm install git@github.com:nflercode/eslint-config-nfler.git  
+```
---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = {
+    rules: {
+        //"prettier/prettier": [
+        //    "error",
+        //    {
+        //        endOfLine: "auto"
+        //    }
+        //],
+        "no-undef": "error",
+        "no-empty-function": "error",
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+        "react/jsx-no-duplicate-props": "error",
+        "react/jsx-key": "error",
+        "react/require-render-return": "error",
+        "react/react-in-jsx-scope": "error",
+        "react/prop-types": "off",
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+        "prefer-template": "error"
+    }
+};
